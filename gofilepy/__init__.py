833ee97988947a414e66bd09dea326a359aae70b
garnajee <>
2025-05-28 13:10:40 +0200

    fix: init.py was empty...

---
+++ gofilepy/__init__.py
@@ -0,0 +1,17 @@
+from .gofile import (
+    gofile_upload,
+    upload,
+    opts,
+    main
+)
+
+# (optionnal)
+__version__ = "0.4.0"
+
+# public exports
+__all__ = [
+    'gofile_upload',
+    'upload',
+    'opts', 
+    'main'
+]
